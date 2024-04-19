c3d7b9ea4d18a01e1d5a498a5448cf014d72cec9
Jonathan-Silva <91348218+JonFalleNBR@users.noreply.github.com>
2024-04-19 06:59:36 -0300

    Commit inicial do projeto

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+
+using System.Windows.Threading;
+
+namespace PAC_Man_Game_WPF_MOD_ICT
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+
+        DispatcherTimer gameTimer = new DispatcherTimer();
+
+
+        bool goLeft, goRight, goUp, goDown;
+        bool noLeft, noRight, noUp, noDown;
+
+
+        int speed = 8;
+
+        Rect pacmanHitbox;
+
+        int ghostSpeed = 10;
+        int ghostMoveStep = 130;
+        int currentGhostStep;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            GameSetup();
+        }
+
+        private void CanvasKeyDown(object sender, KeyEventArgs e)
+        {
+           
+        }
+
+        private void GameSetup()
+        {
+
+            MyCanvas.Focus();
+
+            gameTimer.Tick += GameLoop; 
+            gameTimer.Interval = TimeSpan.FromMilliseconds(20);
+            gameTimer.Start();
+            currentGhostStep = ghostMoveStep;
+
+            ImageBrush pacmanImage = new ImageBrush();
+            pacmanImage.ImageSource = new BitmapImage(new Uri("pack://application:,,,/images/pacman.jpg"));
+            pacman.Fill = pacmanImage;
+
+            ImageBrush redGhost = new ImageBrush();
+            redGhost.ImageSource = new BitmapImage(new Uri("pack://application:,,,/images/red.jpg"));
+            redGuy.Fill = redGhost;
+
+            ImageBrush orangeGhost = new ImageBrush();
+            orangeGhost.ImageSource = new BitmapImage(new Uri("pack://application:,,,/images/orange.jpg"));
+            orangeGuy.Fill = orangeGhost;
+
+
+            ImageBrush pinkGhost = new ImageBrush();
+            pinkGhost.ImageSource = new BitmapImage(new Uri("pack://application:,,,/images/pink.jpg"));
+            pinkGuy.Fill = pinkGhost;
+
+
+        }
+
+        private void GameLoop(object sender, EventArgs e)
+        {
+
+        }
+
+        private void GameOver(string message)
+        {
+            gameTimer.Stop();
+            MessageBox.Show(message, "The Pac Man Game MOD ICT");
+
+            System.Diagnostics.Process.Start(Application.ResourceAssembly.Location);
+            Application.Current.Shutdown();
+        }
+
+
+
+    }
+}
